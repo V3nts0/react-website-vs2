56221f3153b611233ee0fce1dea18059231eb544
V3nts0 <ventsi.d.georgiev@gmail.com>
2021-01-14 14:37:18 +0200

    Commit

---
+++ src/components/Cards.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Explore amazing waterfalls deep inside the mountains'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-2.jpg'
+              text='Rest on the stunning Black Sea coast. Experience a great vacation on the natural beaches'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-3.jpg'
+              text='Taste our traditional food, recipes of hundreds of years, aromatic spices, natural products and always cooked with love'
+              label='Delicacy'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-4.jpg'
+              text='Try our traditions. Immerse yourself in the magic of Bulgarian folklore'
+              label='Adventure'
+              path='/products'
+            />
+            </ul>
+            <ul className='cards__items'>
+            <CardItem
+              src='images/img-8.jpg'
+              text='Discover the beauty of architectural and historical landmarks'
+              label='Adventure'
+              path='/services'
+            />
+              <CardItem
+              src='images/img-0.jpg'
+              text='Visit the high-lying lakes at over 2000 m above sea level'
+              label='Adrenaline'
+              path='/services'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/components/HeroSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-3.mp4' autoPlay loop muted />
+      <h1><span class="white">BUL</span><span class="green">GA</span><span class="red">RIA</span>, ADVENTURE <span class="green">AWAITS</span> <span class="red">YOU</span></h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          EXPLORE NOW
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
